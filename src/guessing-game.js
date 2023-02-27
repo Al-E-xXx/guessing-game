4f9a495c9420e4d6f0a4e277fcaaef0257ddd63a
Alex <asgaroth@yandex.ru>
2023-02-27 20:09:06 +0300

    init: Begin my mew task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;        
+        this.result = Math.round(this.max / 2);
     }
 
     guess() {
-
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
+        this.result = Math.round((this.max + this.min) / 2);
     }
 
     greater() {
-
+        this.min = this.result;
+        this.result = Math.round((this.max + this.min) / 2);
     }
 }
 
